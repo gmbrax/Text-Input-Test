ebed26ca5b566640cc0d0366fd599e3d695d6ad9
Gustavo Henrique Santos Souza de MIranda <gustavomiranda@pop-os.localdomain>
2023-07-29 02:36:47 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+from enum import Enum
+
+class States(Enum):
+    FETCH = 0
+    PROCESS = 1
+    EXECUTE = 3
+    EXIT = 4
+
+class StateMachine:
+    def __init__(self):
+        self
+
+if __name__ == "__main__":
+    print("Hellord")
