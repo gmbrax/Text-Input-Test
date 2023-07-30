050b39d4952e431df4d14bf4700e85085f046180
Gustavo Henrique Santos Souza de Miranda <gustavohssmiranda@gmail.com>
2023-07-30 02:52:37 -0300

    Starting the implementation of a class based state machine

--- main.py
+++ main.py
@@ -1,14 +1,43 @@
-from enum import Enum
+from abc import ABC
 
-class States(Enum):
-    FETCH = 0
-    PROCESS = 1
-    EXECUTE = 3
-    EXIT = 4
 
-class StateMachine:
+class InputSystem:
+    _state = None
+
     def __init__(self):
-        self
+        self.transition_to(Fetch())
+
+    def transition_to(self, state):
+        self._state = state
+        self._state.inputsystem = self
+
+
+class State(ABC):
+    @property
+    def context(self):
+        return self._context
+
+    @context.setter
+    def context(self, context):
+        self._context = context
+
+    def change(self, state):
+        # fixMe: This can Lead to infinite recursion error
+        self.context.transition_to(state)
+
+
+class Fetch(State):
+    def __init__(self):
+        print("ping")
+        self.change(Validade())
+
+
+class Validade(State):
+    def __init__(self):
+        print("Pong")
+        self.change(Fetch())
+
 
 if __name__ == "__main__":
     print("Hellord")
+    System = InputSystem()
